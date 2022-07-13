87976aa01e5756f802dd0bbe29dee81b748b86f8
rahul2870 <rahulkashyap2870@gmail.com>
2022-07-14 01:57:54 +0530

    majority elem more than length by 3

---
+++ Arrays - III/majority elem ( more than length by 3 ).java
@@ -0,0 +1,41 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        
+        List<Integer> listToReturn = new ArrayList<>();
+        int num1=-1,num2=-1,count1=0,count2=0,len=nums.length;
+        
+        for(int i=0;i<len;i++){
+            if(nums[i]==num1)count1++;
+            else if(nums[i]==num2)count2++;
+            else if(count1 == 0){
+                num1=nums[i];
+                count1++;
+            }else if(count2 == 0){
+                num2=nums[i];
+                count2++;
+            }else{
+                count1--;
+                count2--;
+            }
+        }
+        
+        count1=0;
+        count2=0;
+         
+        
+        for(int i=0;i<len;i++){
+            if(nums[i] == num1){
+                count1++;
+            }else if(nums[i] == num2){
+                count2++;
+            }
+        }
+        
+        if(count1>len/3)
+            listToReturn.add(num1);
+        if(count2>len/3)
+            listToReturn.add(num2);
+        
+        return listToReturn;
+    }
+}
